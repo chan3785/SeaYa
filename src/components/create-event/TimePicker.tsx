80abe1e5afbbcff8a6edd021daba4666318847c7
chan3785 <dirtyface1234@naver.com>
2025-05-05 22:13:04 +0900

    pnpm error occured

--- src/components/create-event/TimePicker.tsx
+++ src/components/create-event/TimePicker.tsx
@@ -9,66 +9,41 @@ import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import useDate from "../../context/useDate";
+import { TimePicker12Demo } from "./TimePicker/time-picker-12h-demo";
 
 export default function TimePicker({
   className,
 }: React.HTMLAttributes<HTMLDivElement>) {
   const { dateValue, setDateValue } = useDate();
-
+  const [date, setDate] = React.useState<Date | undefined>();
   return (
     <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] h-[100px] text-left text-muted-foreground font-normal top-0 left-0 bg-seaya-black rounded-[10px] border border-solid border-stroke-gray justify-between hover:bg-transparent hover:text-muted-foreground",
-              !dateValue && "text-muted-foreground"
-            )}
-          >
-            <Clock4Icon width={50} height={50} />
-            {dateValue?.from ? (
-              dateValue.to ? (
-                <div className="grid grid-cols-1 gap-2">
-                  <div className="text-xl flex justify-between w-[200px] items-center">
-                    <span className="">Start</span>
-                    <Button
-                      variant={"ghost"}
-                      className="text-xl text-white font-light hover:bg-transparent bg-gray-400/70 w-[140px]"
-                    >
-                      {format(dateValue.from, "EEE, LLL dd")}
-                    </Button>
-                  </div>
-                  <div className="text-xl flex justify-between w-[200px] items-center">
-                    <span className="">End</span>
-                    <Button
-                      variant={"ghost"}
-                      className="text-xl text-white font-light hover:bg-transparent bg-gray-400/70 w-[140px]"
-                    >
-                      {format(dateValue.to, "EEE, LLL dd")}
-                    </Button>
-                  </div>
-                </div>
-              ) : (
-                format(dateValue.from, "EEE, LLL dd")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={dateValue?.from}
-            selected={dateValue}
-            onSelect={setDateValue}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
+      <Button
+        id="date"
+        variant={"outline"}
+        className={cn(
+          "w-[300px] h-[100px] text-left text-muted-foreground font-normal top-0 left-0 bg-seaya-black rounded-[10px] border border-solid border-stroke-gray justify-between hover:bg-transparent hover:text-muted-foreground",
+          !dateValue && "text-muted-foreground"
+        )}
+      >
+        <Clock4Icon width={50} height={50} />
+        <div className="grid grid-cols-1 gap-2">
+          <div className="text-xl flex justify-between w-[200px] items-center">
+            <span className="">Start</span>
+            <TimePicker12Demo date={date} setDate={setDate} />
+            {/* {format(dateValue.from, "h:mm a")} */}
+          </div>
+          <div className="text-xl flex justify-between w-[200px] items-center">
+            <span className="">End</span>
+            <Button
+              variant={"ghost"}
+              className="text-xl text-white font-light hover:bg-transparent bg-gray-400/70 w-[140px]"
+            >
+              {/* {format(dateValue.to, "h:mm a")} */}
+            </Button>
+          </div>
+        </div>
+      </Button>
     </div>
   );
 }
