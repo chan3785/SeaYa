687447e228e3173e39e2339e5eced7424913675b
chan3785 <dirtyface1234@naver.com>
2025-05-12 00:48:29 +0900

    create projects page modified: category field add & Projects Tab list modified: Design to Games

--- src/components/create-projects/CreateProjects.tsx
+++ src/components/create-projects/CreateProjects.tsx
@@ -4,7 +4,14 @@ import DatePicker from "../create-event/DatePicker";
 import useDate from "../../context/useDate";
 import TimePicker from "../create-event/TimePicker";
 import TimeZonePicker from "../create-event/TimezonePicker";
-import { FileText, MapPin, Palette, Tickets, UserRound } from "lucide-react";
+import {
+  FileText,
+  MapPin,
+  Palette,
+  Tag,
+  Tickets,
+  UserRound,
+} from "lucide-react";
 import Footer from "../Footer";
 
 interface FormData {
@@ -15,6 +22,7 @@ interface FormData {
   endTime: string;
   timeZone: string;
   location: string;
+  category: string;
   description: string;
   ticketsOption?: string;
   eventImage?: File | null;
@@ -29,6 +37,7 @@ export default function CreateProjects() {
     endTime: "",
     timeZone: "",
     location: "",
+    category: "",
     description: "",
     ticketsOption: "",
     eventImage: null,
@@ -96,6 +105,22 @@ export default function CreateProjects() {
                 placeholder="Add location"
                 className="w-full p-2 pl-8 bg-gray-700 text-muted-foreground top-0 left-0 bg-seaya-black rounded-[10px] border border-solid border-stroke-gray"
               />
+            </div>
+            <div className="relative flex items-center border border-muted-foreground rounded-[10px]">
+              <span className="absolute left-1 top-1/2 transform -translate-y-1/2">
+                <Tag className="text-muted-foreground mx-1" />
+              </span>
+              <select
+                name="category"
+                className="p-2 rounded bg-gray-700 text-muted-foreground w-[810px] bg-transparent flex items-end ml-6"
+                onChange={handleChange}
+              >
+                <option value="">Select Category</option>
+                <option value="hackathon">Hackathon</option>
+                <option value="development">Development</option>
+                <option value="games">Games</option>
+                <option value="others">Others</option>
+              </select>
             </div>
             <div className="relative flex items-center">
               <span className="absolute left-1 top-1/2 transform -translate-y-1/2">
